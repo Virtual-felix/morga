4ceeb6efc55b1884917cb949abeb2d28fb6a9055
ffabrega <ffabrega@ffabrega>
2020-08-12 12:24:39 +0200

    run script to avoid shell clear + request on API

--- app/src/app.js
+++ app/src/app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import Box from '@material-ui/core/Box';
 import logo from './logo512white.png';
 import { ThemeProvider } from "@material-ui/styles";
@@ -6,6 +7,14 @@ import { theme } from './theme';
 import "typeface-roboto";
 
 function App() {
+  axios.get(process.env.REACT_APP_API_URL)
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
   return (
     <ThemeProvider theme={theme}>
       <Box bgcolor="#f5f7f8" minHeight="100vh" textAlign="center" fontFamily="roboto">
